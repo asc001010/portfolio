6b0191c05a105e590eddbc42fe4e7a05117c9cda
안승찬 <115769266+asc001010@users.noreply.github.com>
2026-04-10 19:54:51 +0900

    fix: remove duplicate function definition in RealtimeLockerList.js

--- components/mygo/RealtimeLockerList.js
+++ components/mygo/RealtimeLockerList.js
@@ -153,20 +153,3 @@ function ArrowRightForward({ className }) {
     </svg>
   );
 }
-
-function ArrowRightForward({ className }) {
-  return (
-    <svg 
-      xmlns="http://www.w3.org/2000/svg" 
-      viewBox="0 0 24 24" 
-      fill="none" 
-      stroke="currentColor" 
-      strokeWidth="2" 
-      strokeLinecap="round" 
-      strokeLinejoin="round" 
-      className={className}
-    >
-      <path d="M5 12h14M12 5l7 7-7 7"/>
-    </svg>
-  );
-}
